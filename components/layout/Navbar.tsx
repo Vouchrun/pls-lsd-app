84648ff3b6b2fc50038127d856ee8441dae1b8ed
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2024-07-21 21:49:23 +0800

    app icon

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -15,7 +15,7 @@ import {
 } from "material-ui-popup-state/hooks";
 import Image from "next/image";
 import appIcon from "public/images/appIconDark.png";
-import appIconLight from "public/images/appIconLight.png";
+//import appIconLight from "public/images/appIconLight.png";
 import defaultAvatar from "public/images/default_avatar.png";
 import noticeIcon from "public/images/notice.png";
 import { useEffect, useMemo, useState } from "react";
