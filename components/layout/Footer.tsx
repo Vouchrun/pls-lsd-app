f0f02fa3753ace216e385d06a0c3d5224f19c132
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2024-08-22 16:12:33 +0800

    footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { useAppSlice } from "hooks/selector";
 import Image from "next/image";
-import appLogo from "public/images/stafi_logo.png";
-import appLogoLight from "public/images/stafi_logo_lightmode.png";
+import appLogo from "public/images/logoStafi.svg";
+import appLogoLight from "public/images/logoStafi.svg";
 
 export default function Footer() {
     const { darkMode, unreadNoticeFlag } = useAppSlice();
   return (
     <div className='w-smallContentW xl:w-contentW 2xl:w-largeContentW mx-auto pb-1'>
         <a href='https://www.stafi.io/' target='_blank' className='flex items-center justify-end'>
-            <div style={{ color: darkMode ? "#fff" : "#000" }}>Powered by staFi LSAAS</div>
+            <div style={{ color: darkMode ? "#fff" : "#000" }}>Powered by staFi LSaaS</div>
             <Image
                 src={darkMode ? appLogo : appLogoLight}
                 alt="stafi"
-                width={80}
+                width={30}
                 className='ml-[5px]'
             />
         </a>
