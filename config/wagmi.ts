73deaadd6eb56e065a19be49d1754715da464334
ravirasadiya <ravirasadiya1994@gmail.com>
2024-09-26 11:18:51 +0530

    fix: build error

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,24 +1,24 @@
-import { w3mConnectors, w3mProvider } from "@web3modal/ethereum";
-import { configureChains, createConfig } from "wagmi";
-import { getWagmiChainConfig } from "./env";
+// import { w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+// import { configureChains, createConfig } from 'wagmi';
+// import { getWagmiChainConfig } from './env';
 
-// 1. Get projectId
-export const walletConnectProjectId = "773e240347e5c760d1cc49e512d0d86c";
-// 2. Configure wagmi client
-const chains = [getWagmiChainConfig()];
+// // 1. Get projectId
+// export const walletConnectProjectId = '773e240347e5c760d1cc49e512d0d86c';
+// // 2. Configure wagmi client
+// const chains = [getWagmiChainConfig()];
 
-const { publicClient } = configureChains(chains, [
-  w3mProvider({ projectId: walletConnectProjectId }),
-]);
+// const { publicClient } = configureChains(chains, [
+//   w3mProvider({ projectId: walletConnectProjectId }),
+// ]);
 
-export const wagmiConfig = createConfig({
-  autoConnect: false,
-  connectors: [
-    ...w3mConnectors({
-      // version: 2,
-      chains,
-      projectId: walletConnectProjectId,
-    }),
-  ],
-  publicClient,
-});
+// export const wagmiConfig = createConfig({
+//   autoConnect: false,
+//   connectors: [
+//     ...w3mConnectors({
+//       // version: 2,
+//       chains,
+//       projectId: walletConnectProjectId,
+//     }),
+//   ],
+//   publicClient,
+// });
