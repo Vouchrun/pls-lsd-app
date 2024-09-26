6dd77677e79fe5d3f1304e8e0f1d4f707221018c
ravirasadiya <ravirasadiya1994@gmail.com>
2024-09-26 11:47:59 +0530

    fix: update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -21,23 +21,38 @@ import noticeIcon from 'public/images/notice.png';
 import { useEffect, useMemo, useState } from 'react';
 import {
   disconnectWallet,
+  setMetaMaskAccount,
   setMetaMaskDisconnected,
 } from 'redux/reducers/WalletSlice';
 import { RootState } from 'redux/store';
 import { getAuditList } from 'utils/configUtils';
 import { getChainIcon } from 'utils/iconUtils';
 import { getShortAddress } from 'utils/stringUtils';
+import { useAccount } from 'wagmi';
 
 const Navbar = () => {
   const { unreadNoticeFlag } = useAppSlice();
-
+  const dispatch = useAppDispatch();
   const [noticeDrawerOpen, setNoticeDrawerOpen] = useState(false);
   const [settingsDrawerOpen, setSettingsDrawerOpen] = useState(false);
   const [auditExpand, setAuditExpand] = useState(false);
   const [pageWidth, setPageWidth] = useState(
     document.documentElement.clientWidth
   );
-  const { metaMaskAccount } = useWalletAccount();
+
+  const { isDisconnected, address } = useAccount();
+
+  useEffect(() => {
+    if (isDisconnected) {
+      dispatch(setMetaMaskDisconnected(true));
+      dispatch(setMetaMaskAccount(undefined));
+    } else {
+      if (address) {
+        dispatch(setMetaMaskAccount(address));
+        dispatch(setMetaMaskDisconnected(false));
+      }
+    }
+  }, [isDisconnected, address]);
 
   const resizeListener = () => {
     const clientW = document.documentElement.clientWidth;
@@ -124,154 +139,154 @@ const Navbar = () => {
   );
 };
 
-const UserInfo = (props: { auditExpand: boolean }) => {
-  const { auditExpand } = props;
-  const dispatch = useAppDispatch();
-  const { metaMaskAccount } = useWalletAccount();
-  const { darkMode } = useAppSelector((state: RootState) => {
-    return {
-      darkMode: state.app.darkMode,
-    };
-  });
+// const UserInfo = (props: { auditExpand: boolean }) => {
+//   const { auditExpand } = props;
+//   const dispatch = useAppDispatch();
+//   const { metaMaskAccount } = useWalletAccount();
+//   const { darkMode } = useAppSelector((state: RootState) => {
+//     return {
+//       darkMode: state.app.darkMode,
+//     };
+//   });
 
-  const hideAddress = useMemo(() => {
-    return auditExpand;
-  }, [auditExpand]);
+//   const hideAddress = useMemo(() => {
+//     return auditExpand;
+//   }, [auditExpand]);
 
-  const addressPopupState = usePopupState({
-    variant: 'popover',
-    popupId: 'address',
-  });
+//   const addressPopupState = usePopupState({
+//     variant: 'popover',
+//     popupId: 'address',
+//   });
 
-  return (
-    <div className='h-[.42rem] bg-color-bg2 rounded-[.6rem] flex items-stretch'>
-      <div
-        className={classNames(
-          'items-center pl-[.04rem] pr-[.12rem] rounded-l-[.6rem] cursor-pointer',
-          auditExpand ? 'hidden 2xl:flex' : 'flex'
-        )}
-      >
-        <div className='w-[.34rem] h-[.34rem] relative'>
-          <Image
-            src={getChainIcon()}
-            alt='logo'
-            className='rounded-full  overflow-hidden'
-            layout='fill'
-          />
-        </div>
+//   return (
+//     <div className='h-[.42rem] bg-color-bg2 rounded-[.6rem] flex items-stretch'>
+//       <div
+//         className={classNames(
+//           'items-center pl-[.04rem] pr-[.12rem] rounded-l-[.6rem] cursor-pointer',
+//           auditExpand ? 'hidden 2xl:flex' : 'flex'
+//         )}
+//       >
+//         <div className='w-[.34rem] h-[.34rem] relative'>
+//           <Image
+//             src={getChainIcon()}
+//             alt='logo'
+//             className='rounded-full  overflow-hidden'
+//             layout='fill'
+//           />
+//         </div>
 
-        <div
-          className={classNames('ml-[.08rem] text-[.16rem] text-color-text1')}
-        >
-          {getEthereumChainName()}
-        </div>
+//         <div
+//           className={classNames('ml-[.08rem] text-[.16rem] text-color-text1')}
+//         >
+//           {getEthereumChainName()}
+//         </div>
 
-        {/* <div className="ml-[.12rem]">
-          <Icomoon icon="arrow-down" size=".1rem" color="#848B97" />
-        </div> */}
-      </div>
+//         {/* <div className="ml-[.12rem]">
+//           <Icomoon icon="arrow-down" size=".1rem" color="#848B97" />
+//         </div> */}
+//       </div>
 
-      <div
-        className={classNames(
-          'self-center h-[.22rem] w-[.01rem] bg-[#DEE6F7] dark:bg-[#6C86AD80]',
-          auditExpand ? 'hidden 2xl:flex' : 'flex'
-        )}
-      />
+//       <div
+//         className={classNames(
+//           'self-center h-[.22rem] w-[.01rem] bg-[#DEE6F7] dark:bg-[#6C86AD80]',
+//           auditExpand ? 'hidden 2xl:flex' : 'flex'
+//         )}
+//       />
 
-      <div
-        className={classNames(
-          'cursor-pointer pr-[.04rem] flex items-center rounded-r-[.6rem]',
-          addressPopupState.isOpen ? 'bg-color-selected' : '',
-          auditExpand
-            ? 'rounded-[.6rem] pl-[.04rem] 2xl:rounded-r-[.6rem] 2xl:pl-[.12rem]'
-            : 'rounded-r-[.6rem]  pl-[.12rem]'
-        )}
-        {...bindTrigger(addressPopupState)}
-      >
-        <Image
-          src={defaultAvatar}
-          alt='logo'
-          className='w-[.34rem] h-[.34rem] rounded-full'
-        />
+//       <div
+//         className={classNames(
+//           'cursor-pointer pr-[.04rem] flex items-center rounded-r-[.6rem]',
+//           addressPopupState.isOpen ? 'bg-color-selected' : '',
+//           auditExpand
+//             ? 'rounded-[.6rem] pl-[.04rem] 2xl:rounded-r-[.6rem] 2xl:pl-[.12rem]'
+//             : 'rounded-r-[.6rem]  pl-[.12rem]'
+//         )}
+//         {...bindTrigger(addressPopupState)}
+//       >
+//         <Image
+//           src={defaultAvatar}
+//           alt='logo'
+//           className='w-[.34rem] h-[.34rem] rounded-full'
+//         />
 
-        {!hideAddress && (
-          <div
-            className={classNames(
-              'mx-[.12rem] text-[.16rem]',
-              addressPopupState.isOpen ? 'text-text1 ' : 'text-color-text1'
-            )}
-          >
-            {getShortAddress(metaMaskAccount, 5)}
-          </div>
-        )}
-      </div>
+//         {!hideAddress && (
+//           <div
+//             className={classNames(
+//               'mx-[.12rem] text-[.16rem]',
+//               addressPopupState.isOpen ? 'text-text1 ' : 'text-color-text1'
+//             )}
+//           >
+//             {getShortAddress(metaMaskAccount, 5)}
+//           </div>
+//         )}
+//       </div>
 
-      {/* Address Menu */}
-      <Popover
-        {...bindPopover(addressPopupState)}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        elevation={0}
-        sx={{
-          marginTop: '.15rem',
-          '& .MuiPopover-paper': {
-            background: darkMode ? '#6C86AD4D' : '#ffffff80',
-            border: darkMode
-              ? '0.01rem solid #6C86AD80'
-              : '0.01rem solid #FFFFFF',
-            backdropFilter: 'blur(.4rem)',
-            borderRadius: '.3rem',
-          },
-          '& .MuiTypography-root': {
-            padding: '0px',
-          },
-          '& .MuiBox-root': {
-            padding: '0px',
-          },
-        }}
-      >
-        <div
-          className={classNames('p-[.16rem] w-[2rem]', darkMode ? 'dark' : '')}
-        >
-          <div
-            className='cursor-pointer flex items-center justify-between'
-            onClick={() => {
-              navigator.clipboard.writeText(metaMaskAccount || '').then(() => {
-                addressPopupState.close();
-              });
-            }}
-          >
-            <div className='flex items-center'>
-              <div className='ml-[.12rem] text-color-text1 text-[.16rem]'>
-                Copy Address
-              </div>
-            </div>
-          </div>
+//       {/* Address Menu */}
+//       <Popover
+//         {...bindPopover(addressPopupState)}
+//         anchorOrigin={{
+//           vertical: 'bottom',
+//           horizontal: 'right',
+//         }}
+//         transformOrigin={{
+//           vertical: 'top',
+//           horizontal: 'right',
+//         }}
+//         elevation={0}
+//         sx={{
+//           marginTop: '.15rem',
+//           '& .MuiPopover-paper': {
+//             background: darkMode ? '#6C86AD4D' : '#ffffff80',
+//             border: darkMode
+//               ? '0.01rem solid #6C86AD80'
+//               : '0.01rem solid #FFFFFF',
+//             backdropFilter: 'blur(.4rem)',
+//             borderRadius: '.3rem',
+//           },
+//           '& .MuiTypography-root': {
+//             padding: '0px',
+//           },
+//           '& .MuiBox-root': {
+//             padding: '0px',
+//           },
+//         }}
+//       >
+//         <div
+//           className={classNames('p-[.16rem] w-[2rem]', darkMode ? 'dark' : '')}
+//         >
+//           <div
+//             className='cursor-pointer flex items-center justify-between'
+//             onClick={() => {
+//               navigator.clipboard.writeText(metaMaskAccount || '').then(() => {
+//                 addressPopupState.close();
+//               });
+//             }}
+//           >
+//             <div className='flex items-center'>
+//               <div className='ml-[.12rem] text-color-text1 text-[.16rem]'>
+//                 Copy Address
+//               </div>
+//             </div>
+//           </div>
 
-          <div className='my-[.16rem] h-[0.01rem] bg-color-divider1' />
+//           <div className='my-[.16rem] h-[0.01rem] bg-color-divider1' />
 
-          <div
-            className='cursor-pointer flex items-center justify-between'
-            onClick={() => {
-              addressPopupState.close();
-              dispatch(disconnectWallet());
-            }}
-          >
-            <div className='ml-[.12rem] text-color-text1 text-[.16rem]'>
-              Disconnect
-            </div>
-          </div>
-        </div>
-      </Popover>
-    </div>
-  );
-};
+//           <div
+//             className='cursor-pointer flex items-center justify-between'
+//             onClick={() => {
+//               addressPopupState.close();
+//               dispatch(disconnectWallet());
+//             }}
+//           >
+//             <div className='ml-[.12rem] text-color-text1 text-[.16rem]'>
+//               Disconnect
+//             </div>
+//           </div>
+//         </div>
+//       </Popover>
+//     </div>
+//   );
+// };
 
 // const ConnectButton = () => {
 //   const dispatch = useAppDispatch();
