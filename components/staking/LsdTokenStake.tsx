dd3e6bf3825ad66d2a529566fb48da06a2a7bc99
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2025-09-15 10:35:29 +0800

    improve APR warning inc version

--- components/staking/LsdTokenStake.tsx
+++ components/staking/LsdTokenStake.tsx
@@ -323,9 +323,9 @@ export const LsdTokenStake = () => {
 
       {apr && apr > thresholdAPR ? (
         <div className="threshold-alert text-color-text1 font-[700]">
-          Warning APR appears to be High, (this may be normal).
+          Warning APR appears to be high (this may be normal).
           <br />
-          Exercise some caution if this seems excessive compared to recent APRs.
+          Exercise caution if this seems excessive compared to recent APRs.
           <br />
           If APR is unrealistically high you will get less vPLS when staking PLS.
         </div>
